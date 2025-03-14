324da89acb79a4b74fa02c343d1f35ff68957b69
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-03-14 01:13:16 +0100

    [FEATURE] Delete templates.

--- templates/client/auth/auth.js
+++
@@ -1,9 +0,0 @@
-function showAuthForm(form) {
-    for (const element of document.getElementById("auth-type-form").children) {
-        if (element.id == form) {
-            element.classList.add("show");
-            continue;
-        }
-        element.classList.remove("show");
-    }
-}
