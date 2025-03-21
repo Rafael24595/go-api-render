55571f3b254ed18bc21a003421d8a85ea4df2e6b
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2025-03-21 22:44:31 +0100

    [FEATURE] Implement context management in ControllerActions.

--- src/infrastructure/controller/ControllerActions.go
+++ src/infrastructure/controller/ControllerActions.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/Rafael24595/go-api-core/src/domain/context"
 	core_infrastructure "github.com/Rafael24595/go-api-core/src/infrastructure"
+	"github.com/Rafael24595/go-api-core/src/infrastructure/dto"
 	"github.com/Rafael24595/go-api-core/src/infrastructure/repository"
 	"github.com/Rafael24595/go-api-render/src/infrastructure/router"
 )
@@ -28,19 +30,22 @@ func NewControllerActions(router *router.Router, repository *repository.RequestM
 	return instance
 }
 
-func (c *ControllerActions) action(w http.ResponseWriter, r *http.Request, context router.Context) error {
-	actionRequest, err := jsonDeserialize[RequestExecuteAction](r)
+func (c *ControllerActions) action(w http.ResponseWriter, r *http.Request, ctx router.Context) error {
+	actionData, err := jsonDeserialize[RequestExecuteAction](r)
 	if err != nil {
 		return err
 	}
 
-	actionResponse, err := core_infrastructure.Client().Fetch(actionRequest.Request)
+	actionContext := dto.ToContext(&actionData.Context)
+	actionRequest := context.ProcessRequest(&actionData.Request, actionContext)
+
+	actionResponse, err := core_infrastructure.Client().Fetch(*actionRequest)
 	if err != nil {
 		return err
 	}
 
 	response := responseAction{
-		Request:  actionRequest.Request,
+		Request:  *actionRequest,
 		Response: *actionResponse,
 	}
 
